cd1bcbd32fa21217a7fe946e9a18cab669044f7c
Darkdevolt <diopibrahimabdallah@gmail.com>
2026-08-18 17:03:01 +0000

    fix(admin): load session details actions reliably

--- public/js/seances-globales.js
+++ public/js/seances-globales.js
@@ -1,8 +1,12 @@
 /* Loader de compatibilité pour le module de gestion globale des séances. */
 (function(){
-  var s=document.createElement('script');
-  s.src='/admin/js/seances-globales.js?v=20260816-3';
-  s.defer=true;
-  s.onerror=function(){console.warn('[seances-globales] module admin introuvable')};
-  document.head.appendChild(s);
+  function load(src){
+    var s=document.createElement('script');
+    s.src=src;
+    s.defer=true;
+    s.onerror=function(){console.warn('[seances-globales] module introuvable: '+src)};
+    document.head.appendChild(s);
+  }
+  load('/admin/js/seances-details.js?v=20260818-1');
+  load('/admin/js/seances-globales.js?v=20260818-1');
 })();
